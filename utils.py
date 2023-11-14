637c869b39f43695b685b1fff5ccd059644a689b
Hryhorzhevska <anastasiia.hryhorzhevska@nrcan-rncan.gc.ca>
2023-11-14 17:25:08 -0500

    Function for configuring logging to write both to the console and a file

---
+++ utils.py
@@ -0,0 +1,42 @@
+# utils.py
+
+import logging
+import sys
+from datetime import datetime
+
+def configure_logging():
+    """
+    Configures logging to write both to the console and a file.
+
+    Returns:
+        None
+    """
+    # Create a log file with a timestamp
+    log_file = f"logs/nlp_flex_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+    logging.basicConfig(level=logging.INFO)
+
+    # Create a file handler and set the logging level to WARNING
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.WARNING)
+
+    # Create a console handler and set the logging level to INFO
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Set the formatter for both handlers
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Get the root logger
+    logger = logging.getLogger()
+
+    # Add both handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    # Return the log file path
+    return log_file
