cda9a2116b1671490a18749463c06520c77e3fe2
Hryhorzhevska <anastasiia.hryhorzhevska@nrcan-rncan.gc.ca>
2023-11-21 16:27:40 -0500

    adjustment for multiprocessing log output to a file

--- content_extractor.py
+++ content_extractor.py
@@ -2,6 +2,7 @@
 
 import re
 import os
+from os import path
 import json
 import pandas as pd
 import requests
@@ -13,10 +14,11 @@ from nltk.tokenize import word_tokenize
 import multiprocessing
 import openai
 import logging
+import logging.handlers
 from datetime import datetime
 from signal import signal, SIGINT
 
-from utils import handler
+from utils import handler, listener_configurer, listener_process, LOG_FILE_PATH, LOG_NAME
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -133,17 +135,35 @@ class ContentExtractor:
             print(f"An error occurred while removing stopwords: {e}")
             return text  # Return the original text in case of an error
 
-    def extract_url_content(self, url, language='en'):
+    def extract_url_content(self, url, log_queue, i, language='en'):
         """Extracts content from a given URL.
 
         Args:
             url (str): URL to extract content from.
             language (str, optional): Language of the content. Defaults to 'en'.
+            log_queue (multiprocessing.Queue): A queue used for communication between the main process and the logging listener process.
+            i (int): An integer representing the index or identifier of the current process.
 
         Returns:
             tuple: Summary, content, and validity flag (1 if valid body, 0 otherwise).
         """
         signal(SIGINT, handler)
+        
+        # Create a QueueHandler to enable logging to the shared log_queue
+        qh = logging.handlers.QueueHandler(log_queue)
+        
+        # Get or create the root logger, and add the QueueHandler to it
+        root_logger = logging.getLogger(path.join(LOG_FILE_PATH, LOG_NAME))
+        root_logger.addHandler(qh)
+        
+        # Set the root logger to log info including warnings and errors
+        root_logger.setLevel(logging.INFO)
+
+        # Create a child logger for this specific process
+        logger = logging.getLogger(LOG_NAME).getChild(f'child_{i}') # This allows you to know what process you're in
+        
+        # Set the child logger to log info including warnings and errors
+        logger.setLevel(logging.INFO)
         
         summary, content, is_valid = '', '', 0
 
@@ -157,7 +177,8 @@ class ContentExtractor:
         try:
             # Use newspaper library to extract content from the HTML
             article = self.extract_article(url, response, language)
-
+            logging.info(f"{url}: content extracted")
+            
             # Process and clean the extracted content
             summary = self.clean_text(article.summary)
             content = self.clean_text(article.text)
@@ -219,11 +240,31 @@ class ContentExtractor:
             pd.DataFrame: Dataframe with extracted content.
         """
         try:
+            # Configure the logger for the main process
+            logger = listener_configurer(LOG_NAME, LOG_FILE_PATH)
+
+            # Create a multiprocessing Manager and a Queue for log communication
+            manager = multiprocessing.Manager()
+            log_queue = manager.Queue()
+            # Start the logging listener process in the background
+            listener = multiprocessing.Process(target=listener_process,
+                                               args=(log_queue, listener_configurer, LOG_NAME))
+            listener.start()
+
             if num_processes is None:
                 num_processes = multiprocessing.cpu_count() - 1
-
+                
             with multiprocessing.Pool(processes=num_processes) as pool:
-                results = pool.starmap(self.extract_url_content, zip(df['URL'], df['Language']))
+                logger.info('hello from before process')
+                # Create a list of parameters to process
+                params = []
+                for i, data in enumerate(df['URL']):
+                    params.append((data, log_queue, i))
+            
+                # params = [(df['URL'], df['Language'], log_queue, i) for i in enumerate(df['URL'])]
+                results = pool.starmap(self.extract_url_content, params)
+
+                # results = pool.starmap(self.extract_url_content, zip(df['URL'], df['Language']))  
 
         except KeyboardInterrupt:
             logging.error('Got ^C while pool mapping, terminating the pool')
@@ -253,6 +294,12 @@ class ContentExtractor:
             logging.error(f"An error occurred: {str(e)}")
             logging.error("Data aren't saved but returned")
             pass
+
+        # Signal the end of the queue to stop the logging listener
+        log_queue.put_nowait(None)
+
+        # Wait for the logging listener process to complete
+        listener.join()
 
         return df
 
--- utils.py
+++ utils.py
@@ -1,48 +1,104 @@
 # utils.py
 
 import logging
+import logging.handlers
+from os import path
 import sys
+import traceback
 from datetime import datetime
 from signal import signal, SIGINT
+
+LOG_FILE_PATH='logs'
+LOG_NAME='nlp_flex'
 
 def handler(signalnum, frame):
     signame = SIGINT.name
     print(f'Signal handler called with signal {signame} ({signalnum})')
     raise TypeError
 
-def configure_logging():
+def listener_configurer(log_name, log_file_path):
+    """ Configures and returns a log file based on the given name
+
+    Arguments:
+        log_name (str): String of the log name to use
+        log_file_path (str): String of the log file path
+
+    Returns:
+        logger: configured logging object
     """
-    Configures logging to write both to the console and a file.
+    logger = logging.getLogger(log_name)
+
+    fh = logging.FileHandler(
+        path.join(log_file_path, f'{log_name}.log')) #_{datetime.now().strftime('%Y-%m-%d')}.log'), encoding='cp1252')
+    fmtr = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(fmtr)
+    logger.setLevel(logging.INFO)
+    # current_fh_names = [fh.__dict__.get(
+    #    'baseFilename', '') for fh in logger.handlers]
+    # if not fh.__dict__['baseFilename'] in current_fh_names: # This prevents multiple logs to the same file
+    logger.addHandler(fh)
+
+    return logger
+
+def listener_process(queue, configurer, log_name):
+    """ Listener process is a target for a multiprocess process that runs and listens to a queue for logging events.
+
+    Arguments:
+        queue (multiprocessing.manager.Queue): queue to monitor
+        configurer (func): configures loggers
+        log_name (str): name of the log to use
 
     Returns:
         None
     """
-    # Create a log file with a timestamp
-    log_file = f"logs/nlp_flex_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+    configurer(log_name, LOG_FILE_PATH)
 
-    logging.basicConfig(level=logging.INFO)
+    while True:
+        try:
+            record = queue.get()
+            if record is None:
+                break
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
+        except Exception:
+            print('Failure in listener_process', file=sys.stderr)
+            traceback.print_last(limit=1, file=sys.stderr)
 
-    # Create a file handler and set the logging level to INFO
-    file_handler = logging.FileHandler(log_file, encoding='cp1252')
-    file_handler.setLevel(logging.INFO)
+# def configure_logging():
+#     """
+#     Configures logging to write both to the console and a file.
 
-    # Create a console handler and set the logging level to INFO
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
+#     Returns:
+#         None
+#     """
+#     # Create a log file with a timestamp
+#     log_file = f"logs/nlp_flex_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
-    # Create a formatter
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+#     # Log everything to file
+#     logging.basicConfig(level=logging.INFO)
 
-    # Set the formatter for both handlers
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+#     # Create a file handler and set the logging level to INFO
+#     file_handler = logging.FileHandler(log_file, encoding='cp1252')
+#     file_handler.setLevel(logging.INFO)
 
-    # Get the root logger
-    logger = logging.getLogger()
+#     # Create a console handler and set the logging level to INFO
+#     console_handler = logging.StreamHandler(sys.stdout)
+#     console_handler.setLevel(logging.INFO)
 
-    # Add both handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+#     # Create a formatter
+#     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-    # Return the log file path
-    return log_file
+#     # Set the formatter for both handlers
+#     file_handler.setFormatter(formatter)
+#     console_handler.setFormatter(formatter)
+
+#     # Get the root logger
+#     logger = logging.getLogger()
+
+#     # Add both handlers to the logger
+#     logger.addHandler(file_handler)
+#     logger.addHandler(console_handler)
+
+#     # Return the log file path
+#     return log_file
