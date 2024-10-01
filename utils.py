0331e084f481743c9b1cf8e2877c1bc00cd90ea0
Anastasiia Hryhorzhevska <develop.avec.moi@gmail.com>
2024-10-01 11:55:12 -0400

    Update utils.py with the functions to check and correct json format

--- utils.py
+++ utils.py
@@ -72,3 +72,43 @@ LOGGING_CONFIG = {
         "level": "INFO",
     },
 }
+
+def check_brackets_balance(s):
+    # Dictionary to hold matching brackets
+    brackets = {'(': ')', '{': '}', '[': ']'}
+    open_brackets = brackets.keys()
+    close_brackets = brackets.values()
+    stack = []
+
+    for char in s:
+        if char in open_brackets:
+            stack.append(char)
+        elif char in close_brackets:
+            if not stack:
+                return False, None  # Unmatched closing bracket
+            top = stack.pop()
+            if brackets[top] != char:
+                return False, None  # Mismatched brackets
+
+    # If stack is empty, all brackets are matched
+    if stack:
+        return False, stack  # Return unmatched opening brackets
+    return True, None
+
+def correct_brackets(s):
+    is_balanced, unmatched = check_brackets_balance(s)
+    
+    if is_balanced:
+        return s  # String is already balanced
+
+    # Constructing the corrected string
+    corrected = s
+    for bracket in unmatched:
+        if bracket == '(':
+            corrected += ')'
+        elif bracket == '{':
+            corrected += '}'
+        elif bracket == '[':
+            corrected += ']'
+    
+    return corrected
